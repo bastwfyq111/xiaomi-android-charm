5c71ff1c865d38f6a144cdec150e90caa4406e00
bastwfyq111 <bastwfyq111@gmail.com>
2026-08-15 23:43:12 +0300

    update table.tsx

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -60,7 +60,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+    "h-auto px-1 py-0.5 text-left align-middle font-medium text-muted-foreground text-[10px] sm:text-xs [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className,
     )}
     {...props}
@@ -75,7 +75,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "px-1 py-0.5 align-middle text-[10px] sm:text-xs [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className,
     )}
     {...props}
